0ba8450dca65c698ccc601be1513f95c35ac0bbc
kitaz <kirillpankrasov@kitazs-MacBook-Pro.local>
2024-08-22 01:13:21 +0300

    mm

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(cors());  // Применяем middleware для разрешения CORS
+
 app.get('/api/check', (req, res) => {
     // Логика проверки
     const isValid = true; // Или false, в зависимости от логики
